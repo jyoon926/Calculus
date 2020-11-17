39ec4eda18d4de0b1415d8cac83e78ee19b539fd
Jacob Yoon <jacobyoon926@gmail.com>
2020-11-17 11:00:37 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+function SetType() {
+    var e = document.getElementById("type");
+    var s = e.options[e.selectedIndex].text;
+    if (s == "LRAM") {
+        document.getElementById("calculatelram").style.display = "flex";
+        document.getElementById("calculatemram").style.display = "none";
+        document.getElementById("calculaterram").style.display = "none";
+    }
+    else if (s == "MRAM") {
+        document.getElementById("calculatelram").style.display = "none";
+        document.getElementById("calculatemram").style.display = "flex";
+        document.getElementById("calculaterram").style.display = "none";
+    }
+    else if (s == "RRAM") {
+        document.getElementById("calculatelram").style.display = "none";
+        document.getElementById("calculatemram").style.display = "none";
+        document.getElementById("calculaterram").style.display = "flex";
+    }
+}
+
+function calculateLRAM() {
+    LRAM(this.left.value, this.right.value, this.n.value, this.function.value);
+}
+
+function calculateMRAM() {
+    MRAM(this.left.value, this.right.value, this.n.value, this.function.value);
+}
+
+function calculateRRAM() {
+    RRAM(this.left.value, this.right.value, this.n.value, this.function.value);
+}
+
+function LRAM(left, right, n, func) {
+    deltaX = (right - left) / n;
+    sum = 0;
+
+    for (i = 0; i < n; i++) {
+        x = left + i * deltaX;
+        value = eval(func);
+        sum += deltaX * value;
+    }
+
+    output(sum);
+}
+
+function MRAM(left, right, n, func) {
+    deltaX = (right - left) / n;
+    sum = 0;
+
+    for (i = 0; i < n; i++) {
+        x = parseFloat(left) + (i * deltaX) + (deltaX / 2);
+        sum += deltaX * Math.sin(x);
+    }
+
+    output(sum);
+}
+
+function RRAM(left, right, n, func) {
+    deltaX = (right - left) / n;
+    sum = 0;
+
+    for (i = 0; i < n; i++) {
+        x = parseFloat(left) + i * deltaX + deltaX;
+        sum += deltaX * Math.sin(x);
+    }
+
+    output(sum);
+}
+
+
+function output(string) {
+    document.getElementById("output").innerHTML = "Output: " + string;
+}
